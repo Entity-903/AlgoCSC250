f9dc069cbcf03546d6e6b6c484f85f9797803a05
Entity-903 <charlesmassie021@gmail.com>
2025-04-05 04:51:49 -0600

    Add project files.

---
+++ MsTestsProject/SortingLibraryTests.cs
@@ -0,0 +1,357 @@
+﻿using SorterLibrary;
+/**
+ * These test use MsTests
+ *  //https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert?view=mstest-net-3.8
+    //https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert.throws?view=mstest-net-3.8
+ */
+
+namespace MsTestsProject
+{   
+    [TestClass]
+    public class SorterTests
+    {
+        #region Generation Code
+        private static readonly int Seed = 12271978;
+
+        public static int[] GenerateRandom(int size)
+        {
+            var rand = new Random(Seed);
+            return Enumerable.Range(0, size)
+                             .Select(_ => rand.Next(100001))
+                             .ToArray();
+        }
+
+        public static int[] GenerateAscending(int size)
+        {
+            return GenerateRandom(size).OrderBy(x => x).ToArray();
+        }
+
+        public static int[] GenerateDescending(int size)
+        {
+            return GenerateRandom(size).OrderByDescending(x => x).ToArray();
+        }
+
+        public static Person[] GeneratePersonList(int size, PersonOrder order)
+        {
+            return PersonGenerator.Generate(size, Seed, order);
+        }
+
+		public static int romanToInt(string s)
+		{
+			
+			int result = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] == 'M') { result += 1000; continue; }
+				// There is no need to check if the next character is 'M', since that would equal 'D'. Therefore "DM" is redundant
+				if (s[i] == 'D') { result += 500; continue; }
+
+				if (s[i] == 'C')
+				{
+					if (i != s.Length - 1)
+					{
+						if (s[i + 1] == 'D') { result += 400; i++; continue; }
+						if (s[i + 1] == 'M') { result += 900; i++; continue; }
+					}
+					i += 100; continue;
+				}
+
+				if (s[i] == 'L')
+				{
+					if (i != s.Length - 1)
+					{
+						// No need to check if the next character is 'C'. See s[i] == 'D' for explaination
+						if (s[i + 1] == 'D') { result += 450; i++; continue; }
+						if (s[i + 1] == 'M') { result += 950; i++; continue; }
+					}
+					result += 50; continue;
+				}
+
+				if (s[i] == 'X')
+				{
+					if (i != s.Length - 1)
+					{
+						if (s[i + 1] == 'L') { result += 40; i++; continue; }
+						if (s[i + 1] == 'C') { result += 90; i++; continue; }
+						if (s[i + 1] == 'D') { result += 490; i++; continue; }
+						if (s[i + 1] == 'M') { result += 990; i++; continue; }
+					}
+					result += 10; continue;
+				}
+
+				if (s[i] == 'V')
+				{
+					if (i != s.Length - 1)
+					{
+						// No need to check if the next character is 'X'. See s[i] == 'D' for explaination
+						if (s[i + 1] == 'L') { result += 45; i++; continue; }
+						if (s[i + 1] == 'C') { result += 95; i++; continue; }
+						if (s[i + 1] == 'D') { result += 495; i++; continue; }
+						if (s[i + 1] == 'M') { result += 995; i++; continue; }
+					}
+					result += 5; continue;
+				}
+
+				if (s[i] == 'I')
+				{
+					if (i != s.Length - 1)
+					{
+						if (s[i + 1] == 'V') { result += 4; i++; continue; }
+						if (s[i + 1] == 'X') { result += 9; i++; continue; }
+						if (s[i + 1] == 'L') { result += 49; i++; continue; }
+						if (s[i + 1] == 'C') { result += 99; i++; continue; }
+						if (s[i + 1] == 'D') { result += 499; i++; continue; }
+						if (s[i + 1] == 'M') { result += 999; i++; continue; }
+					}
+					result += 1; continue;
+				}
+				// Character was invalid. Inform user
+				return -1;
+			}
+			return result;
+		}
+
+		#endregion
+
+		[TestMethod]
+        public void ExampleTestMethod()
+        {
+            //Assemble
+            //int[] testArray = { 5, 4, 3, 2, 1 };
+            int[] testArray = GenerateDescending(5);
+            //int[] expectedResult = { 1, 2, 3, 4, 5};
+            int[] expectedResult = GenerateAscending(5);
+
+            //Act
+            Sorter<int>.BubbleSort(testArray);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, testArray);
+
+        }
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		[TestMethod]
+		public void BubbleSortHappyPath()
+		{
+			int[] input    = { 7, 2, 8, 3, 1, 12, 15 };
+			int[] expected = { 1, 2, 3, 7, 8, 12, 15};
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void BubbleSortBestCase()
+		{
+			int[] input =    { 1, 2, 3, 7, 8, 12, 15 };
+			int[] expected = { 1, 2, 3, 7, 8, 12, 15 };
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void BubbleSortWorseCase()
+		{
+			int[] input	   = { 15, 12, 8, 7, 3, 2, 1 };
+			int[] expected = { 1, 2, 3, 7, 8, 12, 15 };
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void BubbleSortNegativeValues()
+		{
+			int[] input    = { -10,  3, 1, -2, 4 };
+			int[] expected = { -10, -2, 1,  3, 4 };
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void BubbleSortDuplicateElements()
+		{
+			int[] input    = { 9, 7, 5, 5, 3, 2, 2, 1 };
+			int[] expected = { 1, 2, 2, 3, 5, 5, 7, 9 };
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		//Test generated via ChatGPT
+		[TestMethod]
+		public void BubbleSortSingleElement()
+		{
+			int[] input = { 42 };
+			int[] expected = { 42 };
+
+			Sorter<int>.BubbleSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		[TestMethod]
+		public void SelectionSortHappyPath()
+		{
+			int[] input    = { 5, 4, 3, 2, 1 };
+			int[] expected = { 1, 2, 3, 4, 5 };
+
+			Sorter<int>.SelectionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void SelectionSortNegativeNumbers()
+		{
+			int[] input    = {  0, -1, -2, -3, -4, -5 };
+			int[] expected = { -5, -4, -3, -2, -1,  0 };
+
+			Sorter<int>.SelectionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void SelectionSortEmptyArray()
+		{
+			int[] input    = { };
+			int[] expected = { };
+
+			Sorter<int>.SelectionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void SelectionSortBestCase()
+		{
+			int[] input    = { 1, 2, 3, 4, 5 };
+			int[] expected = { 1, 2, 3, 4, 5 };
+
+			Sorter<int>.SelectionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void SelectionSortDuplicateNumbers()
+		{
+			int[] input    = { 5, 2, 5, 3, 1, 2, 4 };
+			int[] expected = { 1, 2, 2, 3, 4, 5, 5 };
+
+			Sorter<int>.SelectionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		[TestMethod]
+		public void InsertionSortHappyPath()
+		{
+			int[] input = { 5, 4, 3, 2, 1 };
+			int[] expected = { 1, 2, 3, 4, 5 };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void InsertionSortNegativeNumbers()
+		{
+			int[] input = { 0, -1, -2, -3, -4, -5 };
+			int[] expected = { -5, -4, -3, -2, -1, 0 };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void InsertionSortEmptyArray()
+		{
+			int[] input = { };
+			int[] expected = { };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void InsertionSortSingleElement()
+		{
+			int[] input = { 5 };
+			int[] expected = { 5 };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void InsertionSortBestCase()
+		{
+			int[] input = { 1, 2, 3, 4, 5 };
+			int[] expected = { 1, 2, 3, 4, 5 };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		[TestMethod]
+		public void InsertionSortDuplicateNumbers()
+		{
+			int[] input = { 5, 2, 5, 3, 1, 2, 4 };
+			int[] expected = { 1, 2, 2, 3, 4, 5, 5 };
+
+			Sorter<int>.InsertionSort(input);
+
+			CollectionAssert.AreEqual(expected, input);
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		[TestMethod]
+		public void romanToIntHappypath()
+		{
+			string input = "XCII";
+			int expected = 92;
+			int result = romanToInt(input);
+			Assert.AreEqual(expected, result);
+		}
+
+		[TestMethod]
+		public void romanToIntEmptyString()
+		{
+			string input = "";
+			int expected = 0;
+			int result = romanToInt(input);
+			Assert.AreEqual(expected, result);
+		}
+
+		[TestMethod]
+		public void romanToIntIncorrectCharacter()
+		{
+			string input = "XCIIA";
+			int expected = -1;
+			int result = romanToInt(input);
+			Assert.AreEqual(expected, result);
+		}
+
+	}
+}
---
+++ SorterLibrary/Sorter.cs
@@ -0,0 +1,108 @@
+﻿/*
+ *https://learn.microsoft.com/en-us/dotnet/api/system.icomparable-1?view=net-8.0
+ */
+namespace SorterLibrary
+{
+    public class Sorter<T> where T : IComparable<T>
+    {
+        //Time Complexity:  O(n^2)
+        //Space Complexity: O(1)
+        //Iterates through the array, swapping elements until the array is sorted from least to greatest
+        //Array is sorted when no elements are swapped in an iteration
+        public static void BubbleSort(T[] arr)
+        {
+			//If no swaps are made, the array is sorted
+            //Track the number of iterations
+            int iterations = 0;
+            // Initialize here to make the while statement happy
+			bool sorted = false;
+            do
+            {
+				sorted = true;
+				//Iterate through the entire array, starting at the beginning
+				//At the end of each iteration, iterate through the array.Length - 1 - number of iterations
+                //This ignores the element moved furthest to the right, since it is already sorted
+				for (int i = 0; i < arr.Length - 1 - iterations; i++)
+                {
+                    //Compare elements at index and index + 1
+                    // if first is >, swap
+                    if (arr[i].CompareTo(arr[i + 1]) > 0)
+                    {
+                        T temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        sorted = false;
+                    }
+                }
+            } while (!sorted);
+		}
+
+		//Time Complexity:  O(n^2)
+		//Space Complexity: O(1)
+		//Iterates through the array, finds the smallest element in the array, starting with the first element, and swaps said element with the beginning of the unsorted array
+		//This is repeated until the array is sorted
+		public static void SelectionSort(T[] arr)
+        {
+            int smallestValueAt;
+            //Starting with the first element... 
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                smallestValueAt = i;
+				//Compare the value with the other values in the unsorted array
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    //If smallestValue is greater than compared value, update smallestValueAt
+                    if (arr[smallestValueAt].CompareTo(arr[j]) > 0)
+                    {
+                        smallestValueAt = j;
+                    }
+                }
+				//Swap smallestValue with beginning of unsorted array if smallestValueAt was updated
+				if (smallestValueAt != i)
+				{
+					T temp = arr[i];
+					arr[i] = arr[smallestValueAt];
+					arr[smallestValueAt] = temp;
+				}
+			}
+		}
+
+		//Time Complexity:  O(n^2)
+		//Space Complexity: O(1)
+        //Starting with the second element, check each previous element in the array, starting with the immediate left of the element in question
+        //If the previous element value is less than the current value, place the current element value in (previous element + 1),
+        //otherwise copy the previous element value forward to the next element value
+		public static void InsertionSort(T[] arr)
+		{
+            //Start at the second element
+            //First element of array is considered sorted in this context
+            for (int i = 1; i < arr.Length; i++)
+            {
+                T tempCurrentValue = arr[i];
+                //Check as many previous elements as necessary until a desired condition is met
+                for (int j = 1; j <= i; j++)
+                {
+                    if (tempCurrentValue.CompareTo(arr[i - j]) < 0)
+                    {
+						//Copy the previous element value forward to the next element value
+						arr[i - j + 1] = arr[i - j];
+                        if (i == j)
+                        {
+                            //tempCurrentValue is less than all elements in the sorted list
+							//Store tempCurrentValue to first element
+							arr[0] = tempCurrentValue;
+						}
+					}
+                    else
+                    {
+						//Store tempCurrentValue to arr[previousElement + 1]
+						arr[i - j + 1] = tempCurrentValue;
+                        //No need to check remaining elements in sorted list
+                        break;
+					}
+                }
+            }
+		}
+
+	}
+}
